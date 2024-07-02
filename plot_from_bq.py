e613eeb964cb996cc7bfba6f175fa1d26df2c5ba
alexkraj <33723320+alexkraj@users.noreply.github.com>
2024-07-02 17:07:01 +0100

    Update plot_from_bq.py

--- plot_from_bq.py
+++ plot_from_bq.py
@@ -20,5 +20,5 @@ df['timestamp']=(df['timestamp'] - dt.datetime(1970,1,1)).dt.total_seconds()
 # plots all columns against index
 # scatter plot
 df.plot() 
-df.plot(kind='scatter',x='timestamp',y='x_horizon_request_duration') 
+df.plot(kind='scatter',x='timestamp',y='request_duration') 
 plt.show()
